0f8136219d4271b039ae38aff511258d9a18ba1c
Gonzalo Gutierrez <gonzalo2706@gmail.com>
2026-07-30 11:14:19 -0500

    Configure Tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{html,js,php}",
+    "./index.php",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      // We will extend this theme according to the Figma design
+      colors: {
+        // Analizing the Figma, I define custom colors  
+        'rm-green': '#97ce4c',
+        'rm-dark': '#1a1a2e',
+        'rm-dark-blue': '#16213e',
+        'rm-card-bg': '#0f3460',
+        'rm-alive': '#55cc44',
+        'rm-dead': '#d63d2e',
+        'rm-unknown': '#9e9e9e',
+      },
+    },
   },
   plugins: [],
 }
-
